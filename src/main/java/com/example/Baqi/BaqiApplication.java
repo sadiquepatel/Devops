9106708084b70098e8bb0bda5791a0ae83e44379
Sadique Patel <sadiquepateldev@gmail.com>
2023-07-30 18:38:14 +0530

    first commit

---
+++ src/main/java/com/example/Baqi/BaqiApplication.java
@@ -0,0 +1,13 @@
+package com.example.Baqi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaqiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaqiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Baqi/BaqiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Baqi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaqiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
